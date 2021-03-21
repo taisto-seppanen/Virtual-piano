4c36b8fefe68e07b4f98d1458e58800806a86fc9
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-03-22 01:17:04 +0300

    refactor: change structure of the project

--- script.js
+++ script.js
@@ -1,165 +1,79 @@
-const note_a = new sound("./assets/audio/a.mp3");
-const note_a_sharp = new sound("./assets/audio/a♯.mp3");
-const note_b = new sound("./assets/audio/b.mp3");
-const note_c = new sound("./assets/audio/c.mp3");
-const note_c_sharp = new sound("./assets/audio/c♯.mp3");
-const note_d = new sound("./assets/audio/d.mp3");
-const note_d_sharp = new sound("./assets/audio/d♯.mp3");
-const note_e = new sound("./assets/audio/e.mp3");
-const note_f = new sound("./assets/audio/f.mp3");
-const note_f_sharp = new sound("./assets/audio/f♯.mp3");
-const note_g = new sound("./assets/audio/g.mp3");
-const note_g_sharp = new sound("./assets/audio/g♯.mp3");
-
-function sound(src) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.play = function () {
-    this.sound.play();
-  };
+function play(src) {
+  const audio = new Audio();
+  audio.src = src;
+  audio.currentTime = 0;
+  audio.play();
 }
 
-var mouseDown = 0;
-document.addEventListener("keydown", playKey);
-document.addEventListener("keyup", returnDefaulKey);
-let pianoKeys = document.getElementsByClassName("piano-key");
+window.addEventListener("keydown", (event) => {
+  let key = document.getElementById(event.keyCode);
+  key.classList.add("piano-key-active");
+  var src = `assets/audio/${key.dataset.note}.mp3`;
+  play(src);
+});
 
-function playKey(event) {
-  playing(event.keyCode);
-}
-
-//Обработка движений мыши по клавишам
-for (let i of pianoKeys) {
-  i.mouseout = function () {
-    let keyID = i.id;
-    keyID.style.backgroundColor = "green";
-  };
-
-  i.onmousedown = function () {
-    mouseDown = 1;
-    let keyID = i.id;
-    playing(keyID);
-  };
-  i.onmouseup = function () {
-    mouseDown = 0;
-  };
-
-  i.onmouseover = function () {
-    if (mouseDown) {
-      let keyID = i.id;
-      playing(keyID);
-      keyID = document.getElementById(i.id);
-      keyID.classList.add("piano-key-active");
-    } else {
-      let keyID = document.getElementById(i.id);
-    }
-  };
-
-  i.onmouseout = function () {
-    let keyID = document.getElementById(i.id);
-    if (keyID.classList.contains("piano-key-active")) {
-      keyID.classList.remove("piano-key-active");
-    }
-  };
-}
-
-function returnDefaulKey(event) {
+window.addEventListener("keyup", (event) => {
   try {
     code = event.keyCode;
     code += "";
     let keyID = document.getElementById(code);
-    keyID.classList.toggle("piano-key-active");
+    keyID.classList.remove("piano-key-active");
   } catch {}
-}
+});
 
-function playing(code) {
-  code += "";
-  switch (code) {
-    case "81":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_c_sharp.play();
-      break;
-    case "87":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_d_sharp.play();
-      break;
-    case "69":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_f_sharp.play();
-      break;
-    case "82":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_g_sharp.play();
-      break;
-    case "84":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_a_sharp.play();
-      break;
-    case "65":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_c.play();
-      break;
-    case "83":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_d.play();
-      break;
-    case "74":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_b.play();
-      break;
-    case "68":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_e.play();
-      break;
-    case "70":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_f.play();
-      break;
-    case "71":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_g.play();
-      break;
-    case "72":
-      document.getElementById(code).classList.toggle("piano-key-active");
-      note_a.play();
-      break;
-  }
+var isMouseDown = 0;
+for (let key of document.getElementsByClassName("piano-key")) {
+  key.onmousedown = function () {
+    event.target.classList.add("piano-key-active");
+    isMouseDown = 1;
+    var src = `assets/audio/${key.dataset.note}.mp3`;
+    play(src);
+  };
+  key.onmouseup = function () {
+    isMouseDown = 0;
+  };
+  key.onmouseover = function () {
+    if (isMouseDown) {
+      var src = `assets/audio/${key.dataset.note}.mp3`;
+      play(src);
+      event.target.classList.add("piano-key-active");
+    }
+  };
+  key.onmouseout = function () {
+    if (event.target.classList.contains("piano-key-active")) {
+      event.target.classList.remove("piano-key-active");
+    }
+  };
 }
-
 // full screen button script here
 function fullSreenButtonOn() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
   }
 }
-
+// view mode == notes
 const ViewModeNotes = document.querySelector(".btn-notes");
-
 ViewModeNotes.addEventListener("click", (event) => {
   if (ViewModeletter.classList.contains("btn-active")) {
     ViewModeletter.classList.remove("btn-active");
     ViewModeNotes.classList.add("btn-active");
-    for (let i of pianoKeys) {
-      i.classList.remove("piano-key-letter");
+    for (let key of document.getElementsByClassName("piano-key")) {
+      key.classList.remove("piano-key-letter");
     }
   }
 });
-
+// view mode == letters
 const ViewModeletter = document.querySelector(".btn-letters");
-
 ViewModeletter.addEventListener("click", (event) => {
   if (ViewModeNotes.classList.contains("btn-active")) {
     ViewModeNotes.classList.remove("btn-active");
     ViewModeletter.classList.add("btn-active");
   }
-
-  for (let i of pianoKeys) {
-    if (!i.classList.contains("piano-key-letter")) {
-      i.classList.add("piano-key-letter");
+  for (let key of document.getElementsByClassName("piano-key")) {
+    if (!key.classList.contains("piano-key-letter")) {
+      key.classList.add("piano-key-letter");
     }
   }
 });
